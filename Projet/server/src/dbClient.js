5223278dbfac504336c71e501afd53e2b05c417a
Aurélien Bon <aurelien.bon@me.com>
2022-12-21 15:05:12 +0100

    Update dbClient.js

--- Projet/server/src/dbClient.js
+++ Projet/server/src/dbClient.js
@@ -1,24 +1,20 @@
 var redis = require("redis");
 const configure = require('./configure')
 
-let db = null
-
 const config = configure()
-if(process.env.REDIS_CACHEKEY) {
-  db = redis.createClient(6380, process.env.REDIS_HOST,
-    {auth_pass: process.env.REDIS_CACHEKEY, tls: {servername: process.env.REDIS_HOST},
-    retry_strategy: () => {
-    return new Error("Retry time exhausted")
-  }})
-}
-else{
-  db = redis.createClient({
+console.log("host env: "+process.env.REDIS_HOST)
+console.log("port env: "+process.env.REDIS_PORT)
+console.log("host config: "+config.redis.host)
+console.log("port config: "+config.redis.port)
+const db = redis.createClient({
   host: process.env.REDIS_HOST || config.redis.host,
   port: process.env.REDIS_PORT || config.redis.port,
   retry_strategy: () => {
     return new Error("Retry time exhausted")
   }})
-}
+db.on("error", function (err) {
+  console.log("Error " + err);
+});
 
 
 
